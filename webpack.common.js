a5ee78df4716df799de5538ac65a1f52205d3500
Heiki Riesenkampf <hr1514@ic.ac.uk>
2019-02-22 12:46:34 +0100

    Rebuild everything using webpack setup

---
+++ webpack.common.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [
+    new CleanWebpackPlugin(["dist/*"]),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+      filename: path.resolve(__dirname, "index.html")
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[hash].[ext]",
+              publicPath: "dist"
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
