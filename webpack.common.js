8983807c1007acf2481c284549fc98b00daa2548
Heiki Riesenkampf <heiki@mirage.id>
2019-10-10 01:35:02 +0300

    Update links and add rocketbase

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -9,7 +9,7 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
   },
-  plugins: [new CleanWebpackPlugin(["dist/*"])],
+  plugins: [new CleanWebpackPlugin()],
   module: {
     rules: [
       {
